38b052bc4fc61f5cbc7305cc80f00579f5d898aa
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-04-02 18:04:30 +0530

    expertise and contact update

--- src/javascripts/contact.js
+++ src/javascripts/contact.js
@@ -40,7 +40,7 @@ document.querySelectorAll(".lineanime").forEach(function (i) {
     i.addEventListener("mouseenter", function () {
         gsap.to(i.querySelector(".linei"), {
             scaleX: 1, // Use scaleX directly
-            duration:0.3, // Add duration for smooth animation
+            duration: 0.3, // Add duration for smooth animation
             ease: "power1.out", // Optional easing
         });
     });
@@ -53,7 +53,7 @@ document.querySelectorAll(".lineanime").forEach(function (i) {
             scaleX: 0, // Revert to scaleX(0) on mouse leave
             duration: 0.3,
             ease: "power1.in", // Optional easing,
-            onComplete:()=>{
+            onComplete: () => {
                 gsap.set(i.querySelector(".linei"), {
                     transformOrigin: "left", // Retract towards left
                 })
@@ -119,3 +119,53 @@ document.querySelector(".popup-close").addEventListener("click", function (e) {
         }, "a")
 
 })
+
+
+function clock() {
+
+    const arrows = [...document.querySelectorAll('.arrow')];
+    // const digital = document.querySelector('.digital');
+    setInterval(() => {
+        const date = new Date();
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+        const second = date.getSeconds();
+        const milliSecond = date.getMilliseconds();
+        arrows[0].style.setProperty('--rotate', `${hour * 30 + minute / 2}deg`);
+
+        arrows[1].style.setProperty('--rotate', `${minute * 6 + second / 10}deg`);
+        arrows[2].style.setProperty('--rotate', `${second * 6 + milliSecond / 180}deg`);
+        // digital.textContent = `${('0' + hour).substr(-2)}:${('0' + minute).substr(-2)}:${('0' + second).substr(-2)}`;
+    }, 50);
+
+    const bigTicks = [...document.querySelectorAll('.tick.big')];
+    const smallTicks = [...document.querySelectorAll('.tick.small')];
+    const tinyTicks = [...document.querySelectorAll('.tick.tiny')];
+    for (let i = 0; i < bigTicks.length; i++) {
+        bigTicks[i].style.setProperty('--rotate', ['0deg', '90deg'][i]);
+    }
+    for (let i = 0; i < smallTicks.length; i++) {
+        smallTicks[i].style.setProperty('--rotate', ['30deg', '60deg', '-30deg', '-60deg'][i]);
+    }
+    let j = 0;
+    for (let i = 0; i < tinyTicks.length; i++) {
+        if ((i) % 4 === 0) j++;
+        tinyTicks[i].style.setProperty('--rotate', `${(i + j) * 6}deg`);
+    }
+}
+clock()
+
+document.addEventListener("DOMContentLoaded", function () {
+    const splide = new Splide('.splide', {
+        type: 'loop',
+        drag: true,  // Keep dragging enabled
+        focus: 'center',
+        perPage: 3,
+        gap:"5px",
+        autoScroll: {
+            speed: 1,
+        },
+    });
+
+    splide.mount(window.splide.Extensions); // Only mount once
+});
